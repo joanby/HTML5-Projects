a09e32ed8ea1300f1b0935b2c86decf75083218c
joanby <joan-by@hotmail.com>
2018-02-25 19:14:10 +0100

    Inicio tema Javascript
    Inicio tema Javascript

---
+++ Tema 11/01-functions.js
@@ -0,0 +1,14 @@
+function displayName()
+{
+	document.write("Juan Gabriel Gomila");
+}
+
+function sayHello()
+{
+	document.write("Hola, qué tal? ^_^");
+}
+
+function displayScore(){
+	var playerScore = 2018;
+	document.write("<p>La puntuación del jugador es: " + playerScore + "</p>");
+}
---
+++ Tema 7/js/example.js
@@ -0,0 +1,2 @@
+document.write("<p>Esto viene de un fichero Javascript separado del HTML.</p>");
+document.write("<em>Juan Gabriel, nos lo ha enseñado en su curso fantástico</em>");
