58898bc93b31b94447ed0ad45b36bb8b497bf4c8
joanby <joan-by@hotmail.com>
2018-03-07 11:17:14 +0100

    Contenido de Javascript

--- Tema 11/01-functions.js
+++ Tema 11/01-functions.js
@@ -8,7 +8,26 @@ function sayHello()
 	document.write("Hola, qué tal? ^_^");
 }
 
-function displayScore(){
+function displayScore()
+{
 	var playerScore = 2018;
 	document.write("<p>La puntuación del jugador es: " + playerScore + "</p>");
+}
+
+
+/*
+	FUNCIONES PARAMETRIZADAS
+*/
+
+
+function displayPlayerScore(thePlayerScore)
+{
+	thePlayerScore += 100; //thePlayerScore = thePlayerScore + 100
+	document.write("<b>" + thePlayerScore + "</b><br/>");
+}
+
+function addTwoNumbers(x, y){
+	var addition = x + y;
+	document.write("<b>" + addition + "</b><br/>");
+	return(addition);
 }
